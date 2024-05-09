07739867f46f413aae3a2245a9a08db970220ad3
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-05-09 20:02:20 -0300

    refactor: add logger in database file

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 import { DatabaseEnvs } from './envs';
+import { logger } from '../utils/logger-util';
 
 export class DatabaseConfig {
     async connect(): Promise<void> {
         try {
-            console.log(`Connecting to database URI: ${DatabaseEnvs.URI}`);
+            logger.info(`Connecting to database URI: ${DatabaseEnvs.URI}`);
             await mongoose.connect(DatabaseEnvs.URI, {
                 serverSelectionTimeoutMS: 5000,
                 dbName: DatabaseEnvs.DATABASE,
             });
-            console.log('Database successfully connected!');
+            logger.info('Database successfully connected!');
         } catch (error) {
-            console.log('Could not connect to database:', error);
+            logger.error('Could not connect to database:', error);
             throw error;
         }
     }
