4d174eaa68c5a5d32100e0018a9c9dc2b90707a3
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-05-09 23:57:03 -0300

    feat: create context

---
+++ frontend/src/contexts/filter-context.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ReactNode, createContext, useState } from "react";
+
+export const FilterContext = createContext({
+    search: '',
+    setSearch: (value: string) => {},
+})
+
+interface ProviderProps {
+    children: ReactNode
+}
+
+export function FilterContextProvider({ children }: ProviderProps){
+    const [search, setSearch] = useState('')
+
+    return(
+        <FilterContext.Provider 
+            value={{
+                search,
+                setSearch
+            }}>
+            {children}
+        </FilterContext.Provider>
+    )
+}
