31cc16799e6a12dbb6aacb638144fa7f502f5d7a
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-05-10 11:24:46 -0300

    feat: improve context

--- frontend/src/contexts/filter-context.tsx
+++ frontend/src/contexts/filter-context.tsx
@@ -1,13 +1,10 @@
 "use client"
 
-import { ReactNode, createContext, useState } from "react";
+import { IFilterContextProvides } from "@/types/filter-context";
+import axios from "axios";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
-export const FilterContext = createContext({
-    search: '',
-    setSearch: (value: string) => {},
-    pokemon: [],
-    setPokemon: (value: []) => {},
-})
+export const FilterContext = createContext<IFilterContextProvides | undefined>(undefined);
 
 interface ProviderProps {
     children: ReactNode
@@ -15,16 +12,34 @@ interface ProviderProps {
 
 export function FilterContextProvider({ children }: ProviderProps){
     const [search, setSearch] = useState('');
-    const [pokemon, setPokemon] = useState([]);
+    const [pokemon, setPokemon] = useState({abilities: [ { name: '', url: ''}]});
+
+    const API_URL = process.env.API_URL || 'http://localhost:4000/api/pokemons/';
+
+    async function fetchData () {
+        try {
+            const response = await axios.get(`${API_URL}${search}`);
+            setPokemon(response.data.reponse);
+        } catch (error) {
+            console.error(error, { action: fetchData })
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const provider = {
+        search,
+        setSearch,
+        pokemon,
+        setPokemon,
+        fetchData
+    }
 
     return(
         <FilterContext.Provider 
-            value={{
-                search,
-                setSearch,
-                pokemon,
-                setPokemon
-            }}>
+            value={provider}>
             {children}
         </FilterContext.Provider>
     )
